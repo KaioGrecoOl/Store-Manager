a35de47484f845dbe71dd4168468ce686f49782e
Kaio G <kaiogreco91@gmail.com>
2022-07-02 16:54:12 -0300

    feat(teste): Models teste-realiza o teste da conexão e getAllProducts retorna objeto

---
+++ tests/unit/Products.test..js
@@ -0,0 +1,26 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const connection = require("../../models/connection");
+const productsModels = require('../../models/Products');
+
+describe("Test the file Models, products", () => {
+  const mochpayLoadProducts = [{
+    id: 1,
+    name: "Martelo de Thor",
+  }];
+
+  before(async () => {
+
+    sinon.stub(connection, "execute").resolves(mochpayLoadProducts);
+  });
+
+  after(async () => {
+    connection.execute.restore();
+  });
+
+  it("Verify if its an object", async () => {
+    const response = await productsModels.getAllProductsModels();
+    expect(response).to.be.a("object");
+  });
+});
